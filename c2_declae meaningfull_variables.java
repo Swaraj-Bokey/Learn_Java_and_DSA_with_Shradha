96d34bd4bf7e5030178d082bf3da30a48db2853c
Swaraj-Bokey <swarajbokey15082002@gmail.com>
2025-03-12 13:51:10 +0530

    Create c2_declae meaningfull_variables.java

---
+++ c2_declae meaningfull_variables.java
@@ -0,0 +1,16 @@
+//Try to declare meaningful variables of each type.
+//Eg - a variable named age should be a numeric type (int or float) not byte.
+
+class c2_declare_meaningfull_variables 
+{
+    public static void main(String[] args)    
+    {
+        int age = 15;
+        char letter = 'a';
+        boolean answer = true;
+
+        System.out.println(age);
+        System.out.println(letter);
+        System.out.println(answer);
+    }
+}
